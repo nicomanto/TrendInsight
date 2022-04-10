ddecb9667197cbee235a5a60f56d212352779889
nicomanto <nicomanto49@gmail.com>
2022-04-10 17:28:34 +0200

    #10 add gomail client, move controller in support

--- main.go
+++ main.go
@@ -29,11 +29,17 @@ func main() {
 	flag.Parse()
 	setupConfig(*cfgFile)
 	// create twitter client
-	support.SetupOauth1TwitterClient()
+	support.SetupOauth1TwitterClient(
+		viper.GetString("auth.api_key"),
+		viper.GetString("auth.api_key_secret"),
+		viper.GetString("auth.access_token"),
+		viper.GetString("auth.access_token_secret"))
+	// setup email sender
+	support.SetupMailMsgAndDialer(viper.GetString("mail.sender"), viper.GetString("mail.sender_pwd"))
 	// init trend insight go routine
 	waitingGroup := &sync.WaitGroup{}
 	ctxTrendInsight, ctxTrendInsightCF := context.WithCancel(context.Background())
-	routines.InitTrendInsightRoutine(ctxTrendInsight, waitingGroup, viper.GetDuration("trend_insight_post_minute_interval")*time.Minute)
+	routines.InitTrendInsightRoutine(ctxTrendInsight, waitingGroup, viper.GetDuration("trend_insight_post_minute_interval")*time.Minute, viper.GetStringSlice("mail.recipiens"))
 	// Wait for interrupt signal to gracefully shutdown
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
--- support/oauth1.go
+++ support/oauth1.go
@@ -2,15 +2,14 @@ package support
 
 import (
 	"github.com/dghubble/oauth1"
-	"github.com/spf13/viper"
 )
 
-// getOauth1Token() return oauth1 token from access_token and access_token secret
-func getOauth1Token() *oauth1.Token {
-	return oauth1.NewToken(viper.GetString("auth.access_token"), viper.GetString("auth.access_token_secret"))
+// getOauth1Token return oauth1 token from access_token and access_token secret
+func getOauth1Token(token, tokenSecret string) *oauth1.Token {
+	return oauth1.NewToken(token, tokenSecret)
 }
 
-// getOauth1Config() return oauth1 config from api_key and api_key_secret
-func getOauth1Config() *oauth1.Config {
-	return oauth1.NewConfig(viper.GetString("auth.api_key"), viper.GetString("auth.api_key_secret"))
+// getOauth1Config return oauth1 config from consumer_key and consumer_secret
+func getOauth1Config(consumerKey, consumerSecret string) *oauth1.Config {
+	return oauth1.NewConfig(consumerKey, consumerSecret)
 }
