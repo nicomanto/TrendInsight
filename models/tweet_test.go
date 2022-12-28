e2d57aa2a09d630b492257c8babe55cc36738d8e
nicomanto <nicomanto49@gmail.com>
2022-12-28 10:53:21 +0100

    first tests

---
+++ models/tweet_test.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCorrectTweetStructure(t *testing.T) {
+	r := require.New(t)
+	now := time.Now()
+	tweet := PostedTweet{
+		TrendingTweet: TrendingTweet{
+			Hashtag: "dummyHastagh",
+			Text:    "dummyText",
+			Author:  "dummyAuthor",
+			Lang:    "dummyLang",
+			Likes:   104,
+			Ts:      now,
+			URL:     "dummyURL",
+		},
+		TsPosted: now.Add(time.Hour * 24),
+	}
+	postedString := tweet.PostedTweetToString()
+	likesS := strconv.Itoa(tweet.Likes)
+	r.Contains(postedString, tweet.Hashtag)
+	r.Contains(postedString, tweet.Text[:(len(tweet.Text)/2)]+"...")
+	r.Contains(postedString, tweet.Author)
+	r.NotContains(postedString, tweet.Lang)
+	r.Contains(postedString, likesS)
+	r.Contains(postedString, tweet.Ts.Format(time.RFC822))
+	r.Contains(postedString, tweet.URL)
+}
